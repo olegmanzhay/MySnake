d6808463a309ae6f39a99cfbc15eaa907549bba9
olegmanzhay <oleg.manzhay@yandex.ru>
2019-06-25 19:03:05 +0500

    Процедурное программирование. Функция Draw

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -8,9 +8,30 @@ namespace MySnake
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x">Координата x</param>
+        /// <param name="y">Координата y</param>
+        /// <param name="symbol">Символ</param>
+        static void Draw(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x1 = 1;
+            int y1 = 3;
+            char symbol1 = '*';
+            Draw(x1, y1, symbol1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char symbol2 = '*';
+            Draw(x2, y2, symbol2);
+
             Console.ReadLine();
         }
     }
